f2ec2aee4f8831a7bde285a3fb7584eda84898e6
Pushkar Mahakal <pumahaka@redhat.com>
2020-10-27 17:44:04 +0530

    fix format error

--- mkdir-home.sh
+++ mkdir-home.sh
@@ -1,3 +1,3 @@
 /usr/bin/mkdir /var/atlassian/application-data/jira/$MY_POD_NAME
-printf "%s" "jira.node.id=$MY_POD_NAME" > "/var/atlassian/application-data/jira/$MY_POD_NAME/cluster.properties"
 echo 'jira.shared.home=/var/atlassian/application-data/cluster' >> /var/atlassian/application-data/jira/$MY_POD_NAME/cluster.properties
+printf "%s" "jira.node.id=$MY_POD_NAME" >> "/var/atlassian/application-data/jira/$MY_POD_NAME/cluster.properties"
