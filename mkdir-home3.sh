d9ca2476954974603d09804a2e23bdbb27583fd2
Pushkar Mahakal <pumahaka@redhat.com>
2020-10-29 11:54:56 +0530

    modify dockerfile

--- mkdir-home3.sh
+++ mkdir-home3.sh
@@ -1,7 +1,7 @@
 DIR="/var/atlassian/application-data/jira/jira-main-home"
 
 if [ ! -d "$DIR" ]; then
-  mkdir -p "$DIR" && echo 'jira.shared.home=/var/atlassian/application-data/cluster' >> /var/atlassian/application-data/jira/jira-main-home/cluster.properties && /bin/bash /var/atlassian/application-data/jira/mkdir-home4.sh && MY_POD_NAME="jira-main-home"
+  mkdir -p "$DIR" && echo 'jira.shared.home=/var/atlassian/application-data/cluster' >> /var/atlassian/application-data/jira/jira-main-home/cluster.properties && /bin/bash /var/atlassian/application-data/jira/mkdir-home4.sh && JIRA_HOME=/var/atlassian/application-data/jira/jira-main-home
 else 
   mkdir /var/atlassian/application-data/jira/$MY_POD_NAME && cp -rav $DIR/* /var/atlassian/application-data/jira/$MY_POD_NAME && rm -rf /var/atlassian/application-data/jira/$MY_POD_NAME/cluster.properties  && echo 'jira.shared.home=/var/atlassian/application-data/cluster' > /var/atlassian/application-data/jira/$MY_POD_NAME/cluster.properties && /bin/bash /var/atlassian/application-data/jira/mkdir-home5.sh
   exit 1
